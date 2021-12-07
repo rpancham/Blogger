766101672efb0d5b89acdc54cb13bc7a3628a86a
RASHMIP2691 <rashmip2691@gmail.com>
2021-12-07 18:08:59 +0530

    Second commit

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -13,7 +13,52 @@ public class GreetingResource {
     }
 
 
+    @GET
+    @Path("/getallblog")
+
+    public String getallblog () {
+        return "You are Viewing the list of all the blogs";
+    }
+
+
+    @POST
+    @Path("/postblog")
+    public String postblog() {
+        return "A new Blog is created with content Hello";
+    }
+
+//    @GET
+//    @Path("/getparblog")
+//    public String getparblog () {
+//        return "You are viewing a blog with the id 12";
+//    }
+
+    @GET
+    @Path("/{name}/{id}")
+    public String getparblog (@PathParam("name") String name, @PathParam("id")String id) {
+        return "You are viewing a blog with the id " +name   +id;
+    }
+
+    @PUT
+    @Path("/putblog/{id}/{name}")
+    public String putblog (@PathParam("id") String name, @PathParam("name")String id) {
+        return "The blog has been edited with the "+id   +name ;
+    }
+
+
+    @DELETE
+    @Path("/deleteblog")
+    public String deleteblog (@PathParam("id") String id){
+        return "You deleted the blog with the id" +12;
+    }
+
+
+
+
+
+
+
+
 
 
 }
-
